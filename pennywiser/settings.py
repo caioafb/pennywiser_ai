b9d2c071145dbcc19a35be33e8dfe717877885d7
caioafb <caioafb@gmail.com>
2026-02-24 10:53:51 -0300

    Edit allowed hosts

--- pennywiser/settings.py
+++ pennywiser/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-m*&&q7555v&(8j#ert7sw0s&3e_c3ri%&isp!pjh6j7flo#r52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pennywiser-2kkx.onrender.com',
+]
 
 LOGIN_URL = '/login'
 
